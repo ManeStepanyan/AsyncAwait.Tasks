e8bad93e8b6aafc44e98b92daecbd391c7344a25
Mane Stepanyan <mane_stepanyan@epam.com>
2022-06-15 17:05:05 +0400

    Add project files.

---
+++ AsyncAwait.Task1.CancellationTokens/Calculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncAwait.Task1.CancellationTokens;
+
+internal static class Calculator
+{
+    // todo: change this method to support cancellation token
+    public async static Task<long> Calculate(int n, CancellationToken token)
+    {
+        long sum = 0;
+        //to be able to cance; the operaton
+        await Task.Delay(2000);
+
+        for (var i = 0; i < n; i++)
+        {
+            if (token.IsCancellationRequested)
+            {
+                break;
+            }
+            // i + 1 is to allow 2147483647 (Max(Int32)) 
+            sum = sum + (i + 1);
+            await Task.Delay(10);
+        }
+        return sum;
+    }
+}
